ac89108a35291ddf85b33c5d4e317f4ef5159030
print-agent <print_agent_main@proton.me>
2025-08-02 09:42:19 +0500

    Setup for basic handler functions and server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const port = "4000"
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	log.Printf("Starting server on :%s", port)
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
