6f5a1e673e5cf8baa7aa07ec07f0c275c1e891f5
print-agent <print_agent_main@proton.me>
2025-08-02 14:01:41 +0500

    Commandline flags as variables

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,10 @@ import (
 const port = "4000"
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -19,8 +24,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("Starting server on :%s", port)
+	log.Printf("Starting server on %s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
