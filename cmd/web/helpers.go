cb4362ee5fb3da98d632b65837b351de1c671d55
print-agent <print_agent_main@proton.me>
2025-08-06 15:30:08 +0500

    Automatic Form Parsing

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/go-playground/form/v4"
+	"honnef.co/go/tools/go/ir"
 )
 
 func (app *application) serveError(w http.ResponseWriter, r *http.Request, err error) {
@@ -46,4 +50,23 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
 	}
+}
+
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		var invalidDecodeError *form.InvalidDecoderError
+
+		if errors.As(err, &invalidDecodeError) {
+			panic(err)
+		}
+		return err
+	}
+
+	return nil
 }
